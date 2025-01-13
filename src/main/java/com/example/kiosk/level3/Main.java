9819a5259d2171a277ecbd40fbdb220ff46e8d1c
JiHo Jeon <wjswlgh96@naver.com>
2025-01-13 11:26:39 +0900

    fix: Level2 final 필드로 수정

---
+++ src/main/java/com/example/kiosk/level3/Main.java
@@ -0,0 +1,8 @@
+package com.example.kiosk.level3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/kiosk/level3/MenuItem.java
+++ src/main/java/com/example/kiosk/level3/MenuItem.java
@@ -1,4 +1,4 @@
-package com.example.kiosk.level2;
+package com.example.kiosk.level3;
 
 public class MenuItem {
 
